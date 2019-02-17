7b33c4090afe64ea07a371d492bce0ddc06f0ee4
Gustavo <gustavo.simoes@hotmail.com.br>
2019-02-17 20:10:25 -0300

    Creation of the configuration server project, which aims to centralize the entire configuration of our microservices network in one place.

---
+++ store-config-server/src/main/java/br/com/gustavosimoes/storeconfigserver/StoreConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavosimoes.storeconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreConfigServerApplication.class, args);
+    }
+
+}
---
+++ store-config-server/src/test/java/br/com/gustavosimoes/storeconfigserver/StoreConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.gustavosimoes.storeconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreConfigServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
