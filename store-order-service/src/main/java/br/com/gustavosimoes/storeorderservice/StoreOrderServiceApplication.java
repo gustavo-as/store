2d57ac1434e968431af97da0cab126c3663bf389
Gustavo <gustavo.simoes@hotmail.com.br>
2019-02-25 20:58:00 -0300

    Creation of the project from Order Services

---
+++ store-order-service/src/main/java/br/com/gustavosimoes/storeorderservice/StoreOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavosimoes.storeorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreOrderServiceApplication.class, args);
+    }
+
+}
